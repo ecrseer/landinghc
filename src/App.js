d8b54a0027dbb30d39cbcbfa96818b348a1b6362
ecrseer <juustino@live.com>
2021-07-16 19:17:10 -0300

    visual flex cent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+    function salvaStorage(e){
+        e.preventDefault();
+        console.log(`for ema`)
+    }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Receba as melhores ofertas no seu melhor email!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+
+            <div>Nome: <input type="text" placeholder="Como deseja ser chamado"
+                              className="type"/></div>
+            <div>Email: <input type="email"  className="type"/></div>
+
+        </div>
+          <button type="submit" className="btn" onClick={salvaStorage}>Ficar por dentro</button>
       </header>
+        <section>
+
+        </section>
+      <footer>footerdfooter</footer>
     </div>
   );
 }
