8f32f033e1af287d5fcc81cc601f1c73317b864b
ecrseer <juustino@live.com>
2021-07-17 02:36:43 -0300

    form validate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-    function salvaStorage(e){
-        e.preventDefault();
-        console.log(`for ema`)
+
+    const [dadosUsuario,setDadosUsuario] = useState({nome:'',email:''})
+
+    useEffect(()=>{
+        const dadosDeserializados = JSON.parse(localStorage.getItem('dadosUsuario'))
+        setDadosUsuario(dadosDeserializados)
+        let formulario = document.querySelector('form')
+        formulario.onsubmit = (evento)=>{
+            console.log('lovvs')
+            evento.preventDefault()
+        }
+    },[])
+
+    function atualizadorDados(evento){
+
+        function atualizaEstadoObjeto(objetoAnterior){
+
+            return  {...objetoAnterior,
+
+                [evento.target.name]:evento.target.value }
+        }
+
+        setDadosUsuario(atualizaEstadoObjeto)
+        console.log(dadosUsuario)
     }
+
+    function salvaStorage(e){
+        //e.preventDefault();
+
+        console.log(`for ema`)
+        const dadosSerializados = JSON.stringify(dadosUsuario);
+        localStorage.setItem('dadosUsuario',dadosSerializados)
+
+    }
+//    document.addEventListener('onsub')
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Receba as melhores ofertas no seu melhor email!
         </p>
-        <div>
+        <form id="debg" action="" method="GET">
 
-            <div>Nome: <input type="text" placeholder="Como deseja ser chamado"
-                              className="type"/></div>
-            <div>Email: <input type="email"  className="type"/></div>
+            <div>Nome: <input type="text" placeholder="Como desejar ser chamado"
+                              className="type" name="nome" onChange={atualizadorDados}  /></div>
+            <div>Email: <input type="email"  className="type"
+                               name="email" onChange={atualizadorDados}/></div>
+            <button type="submit" className="btn" >Ficar por dentro</button>
+        </form>
 
-        </div>
-          <button type="submit" className="btn" onClick={salvaStorage}>Ficar por dentro</button>
       </header>
         <section>
 
